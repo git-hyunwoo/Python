f721617c0723566faf7733bfd2a0f478f5b23036
Jack Snider <choiman2021@gmail.com>
2023-01-23 15:49:39 +0900

    format

--- chatper02.py
+++ chatper02.py
@@ -35,8 +35,37 @@ print()
 print('%s %s' % ('one','two'))
 print('{} {}'.format('one','two'))
 
-print('{1} {0}'.format('one','two')) # expected result : two one
+print('{1} {0}'.format('one','two')) # expected result : two one 
 
- 
+print()
+
+# %s
+print('%10s' % ('nice')) # leave 10 spaces from left
+print('{:>10}'.format('nice')) # leave 10 spaces from left
+
+print('%-10s' % ('nice')) # leave 10 spaces from right
+print('{:10}'.format('nice')) # leave 10 spaces from right
+
+print('{:_>10}'.format('nice')) # leave 10 spaces from left with the charactor '_'
+print('{:^10}'.format('nice')) # sort the string in the center between 10 spaces
+
+print( '%.5s' % ( 'nice' ) ) # slice 5 letters from left
+print( '%.5s' % ( 'pythonstudy' ) ) 
+print( '{:10.5}'.format( 'pythonstudy' ) ) # make 10 spaces but slice only 5 letters
 
 
+# %d
+print( '%d %d' % ( 1,2 ) )
+print( '{} {}'.format( 1,2 ) )
+
+print( '%4d' % ( 42 ) )
+print( '{:4d}'.format( 42 ) )
+
+print()
+
+# %f
+print( '%1.8f' % ( 3.14343434343434 ) ) # 정수는 1자리, 소수는 8자리까지
+print( '{:f}'.format( 3.14343434343434 ) )
+
+print( '%06.2f' % ( 3.141592653589793 ) ) # 총 6개 중에서 정수부는 1개, 소수부는 2개
+print( '{:06.2f}'.format( 3.141592653589793 ) )
