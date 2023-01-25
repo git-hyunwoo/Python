8645ace87348d324d4ff0d0ea815d5a3b2ce41fc
Jack Snider <choiman2021@gmail.com>
2023-01-26 00:24:55 +0900

    operator 1/2

---
+++ chapter03-01.py
@@ -0,0 +1,20 @@
+# Chapter03-01
+# Numeric
+
+# int : 정수
+# float : 실수
+# complex : 복소수
+# bool : 불린
+# str : 문자열( 시퀸스 )
+# list : 리스트( 시퀸스 )
+# tuple : 튜플( 시퀸스 )
+# set : 집합
+# dict : 사전
+
+# data type
+str1 = 'Python'
+bool = True
+str2 = 'Anaconda'
+float = 10.0 # 10 == 10.0 False
+int = 7
+list = [ str1, str2 ]
