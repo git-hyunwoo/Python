7cf64c298e1d1e0faa5e937d03c780fa76a84e43
= <choiman2021@gmail.com>
2023-09-11 22:36:58 +0900

    daily commit

--- note/note.py
+++ note/note.py
@@ -1,13 +1,31 @@
-print("==== Program begins ====")
+import numpy as np
 
-try:
-    x = int('29') 
-    print(f'my age is {x}')
-except:
-    print("==== except ====")
-else:
-    print('==== else ====')
-finally:
-    print("==== FINALLY ====")
+# 1차원 배열 생성
+array1 = np.array([1,2,3,4,5])
 
-print("==== Program Ends ====")
+# 2차원 배열 생성
+array2 = np.array([[1,2,3],[4,5,6]])
+
+# 배열의 데이터 타입 지정
+array3 = np.array([1,2,3], dtype = np.float64)
+
+
+# 요소별 덧셈
+result1 = array1 + 2
+print(result1) # 출력 : [3 4 5 6 7]
+
+# 요소별 곱셈
+result2 = array1 * 3
+print(result2) # 출력 : [3 6 9 12 15]
+
+# 배열 간 덧셈
+result3 = array1 + array1
+print(result3) # 출력 : [2 4 6 8 10]
+
+# 행렬 곱셈
+result4 = np.dot(array1, array1)
+print(result4)
+
+
+
+
