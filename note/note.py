31e4af33615b4ac8641cbcbca30efdb7f6530609
= <choiman2021@gmail.com>
2023-09-13 22:59:49 +0900

    daily commit

--- note/note.py
+++ note/note.py
@@ -1,31 +1,10 @@
-import numpy as np
+x = 29
+y = x + 1
 
-# 1차원 배열 생성
-array1 = np.array([1,2,3,4,5])
+print(f'id(x) : {id(x)}')
+print(f'id(y) : {id(y)}')
 
-# 2차원 배열 생성
-array2 = np.array([[1,2,3],[4,5,6]])
-
-# 배열의 데이터 타입 지정
-array3 = np.array([1,2,3], dtype = np.float64)
-
-
-# 요소별 덧셈!
-result1 = array1 + 2
-print(result1) # 출력 : [3 4 5 6 7]
-
-# 요소별 곱셈
-result2 = array1 * 3
-print(result2) # 출력 : [3 6 9 12 15]
-
-# 배열 간 덧셈
-result3 = array1 + array1
-print(result3) # 출력 : [2 4 6 8 10]
-
-# 행렬 곱셈
-result4 = np.dot(array1, array1) # 원소 갯수가 같아야 함
-print(result4) # 출력 : 55
-
-
-
-
+if id(x) == id(y):
+    print("둘의 메모리 주소는 같습니다")
+else:
+    print("둘의 메모리 주소는 같지 않습니다")
