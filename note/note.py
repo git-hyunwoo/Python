55c40bc3e7e12e7ada0445dbc28dced8869f157f
= <choiman2021@gmail.com>
2023-09-14 23:59:54 +0900

    daily commit

--- note/note.py
+++ note/note.py
@@ -1,10 +1,13 @@
 x = 29
 y = x + 1
 
+z = 29
+
 print(f'id(x) : {id(x)}')
 print(f'id(y) : {id(y)}')
+print(f'id(z) : {id(z)}')
 
-if id(x) == id(y):
+if id(x) == id(z):
     print("둘의 메모리 주소는 같습니다")
 else:
     print("둘의 메모리 주소는 같지 않습니다")
