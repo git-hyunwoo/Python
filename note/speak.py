d2f8a1ed89344002c35e735c3e10d65e86b1b4ce
= <choiman2021@gmail.com>
2023-09-21 23:21:00 +0900

    daily commit

--- note/speak.py
+++ note/speak.py
@@ -1,31 +1,21 @@
-import os
+import random
 
-# 경로 요소 결합
-path1 = "C:/Users"
-path2 = "Username"
-full_path = os.path.join(path1, path2)
-print("Joined Path:", full_path)
+# 동일한 시드를 사용하여 난수 생성기 초기화
+random.seed(29)
 
-# 경로 존재 여부 확인
-file_path = "/path/to/your/file.txt"
-if os.path.exists(file_path):
-    print("File exists:", file_path)
-else:
-    print("File does not exist:", file_path)
+# 두 번째 난수 생성
+random_num_1 = random.random()
+random_num_2 = random.random()
 
-# 파일 또는 디렉토리 여부 확인
-if os.path.isfile(file_path):
-    print("It's a file:", file_path)
-elif os.path.isdir(file_path):
-    print("It's a directory:", file_path)
+print(random_num_1)  # 출력: 0.5481190538116991
+print(random_num_2)  # 출력: 0.34583182333805484 
 
-# 파일 이름과 확장자 분리한다
-file_name = "example.txt"
-name, extension = os.path.splitext(file_name)
-print("File Name:", name)
-print("Extension:", extension)
+# 동일한 시드를 다시 사용하여 난수 생성기 초기화
+random.seed(29)
 
-# 절대 경로로 변환한다
-relative_path = "../documents/file.txt"
-absolute_path = os.path.abspath(relative_path)
-print("Absolute Path:", absolute_path)
+# 두 번째 난수 생성 (이전과 동일한 결과를 얻을 것입니다)
+random_num_3 = random.random()
+random_num_4 = random.random()
+
+print(random_num_3)  # 출력: 0.5481190538116991
+print(random_num_4)  # 출력: 0.34583182333805484
